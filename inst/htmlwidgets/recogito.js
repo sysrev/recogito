a74158e8adba1af422510d5e4d57bcca0f520e55
Thomas Luechtefeld <tom@insilica.co>
2022-07-08 21:22:54 -0400

    fixed offset issue

--- inst/htmlwidgets/recogito.js
+++ inst/htmlwidgets/recogito.js
@@ -2,49 +2,34 @@ HTMLWidgets.widget({
   name: 'recogito',
   type: 'output',
   factory: function(el, width, height) {
-    var formatter = function(annotation) {
-      return "tag-".concat(annotation.body[0].value)
-    }
+    var formatter = (anno) => "tag-".concat(anno.body[0].value) 
+
     return {
       renderValue: function(x) {
         el.innerText = x.text;
+        
         var r = Recogito.init({
-          content: el.parentNode.id,
+          content: el.id,
           mode: x.mode,
           formatter: formatter,
-          widgets: [
-            'COMMENT',
-            { widget: 'TAG', vocabulary: x.tags }
-          ],
+          widgets: ['COMMENT', { widget: 'TAG', vocabulary: x.tags }],
           readOnly: false
         });
-        r.on('updateAnnotation', function(a) {
-          Shiny.setInputValue(x.inputId, JSON.stringify(r.getAnnotations()));
-        });
-        r.on('createAnnotation', function(a) {
-          Shiny.setInputValue(x.inputId, JSON.stringify(r.getAnnotations()));
-        });
-        //var toggleModeBtn = document.getElementById('toggle-mode');
-        var toggleModeBtn = document.getElementById(el.id.concat("-toggle"));
-        //var annotationMode = 'ANNOTATION';
-        //r.setMode(annotationMode);
-        annotationMode = toggleModeBtn.innerHTML;
-        if (annotationMode === 'MODE: RELATIONS') {
-          annotationMode = 'RELATIONS';
-        } else  {
-          annotationMode = 'ANNOTATION';
-        }
-        toggleModeBtn.addEventListener('click', function() {
-          if (annotationMode === 'ANNOTATION') {
-            toggleModeBtn.innerHTML = 'MODE: RELATIONS';
-            annotationMode = 'RELATIONS';
-          } else  {
-            toggleModeBtn.innerHTML = 'MODE: ANNOTATION';
-            annotationMode = 'ANNOTATION';
-          }
-          r.setMode(annotationMode);
-        });
-        //r.refresh();
+        
+        set_shiny = () => Shiny.setInputValue(x.inputId, JSON.stringify(r.getAnnotations()))
+        r.on('updateAnnotation', set_shiny)
+        r.on('createAnnotation', set_shiny)
+
+  
+        mode_btn = document.getElementById(el.id.concat("-toggle"))
+        mode = mode_btn.innerHTML === 'MODE: RELATIONS' ? "RELATIONS" : "ANNOTATION"
+
+        mode_btn.addEventListener('click', () => {
+          mode_btn.innerHTML = mode === 'ANNOTATION' ? 'MODE: RELATIONS' : 'MODE: ANNOTATION'
+          mode = mode === 'ANNOTATION' ? 'RELATIONS' : 'ANNOTATION'
+          r.setMode(mode);
+        })
+        
       },
       resize: function(width, height) {
       }
